c19689b174b7f253c7613ba7487597010dbaf69b
Enrico <86821724+enricogarozzo@users.noreply.github.com>
2023-03-27 15:53:46 +0200

    created assets folder

--- src/components/FilterActivities.tsx
+++ src/components/FilterActivities.tsx
@@ -2,7 +2,16 @@ import { SearchIcon } from "@chakra-ui/icons";
 import { Box, Flex, Heading, IconButton, Input } from "@chakra-ui/react";
 
 const FilterActivities = () => {
-  
+  // const getLocation = async () => {
+  //   const options = { method: "GET", headers: { accept: "application/json" }};
+  //   fetch(
+  //     "https://api.content.tripadvisor.com/api/v1/location/search?key=7C23E6E7D20B4DCF96588CBA6859738D&searchQuery=siracusa&language=en",
+  //     options
+  //   )
+  //     .then((response) => response.json())
+  //     .then((response) => console.log(response))
+  //     .catch((err) => console.error(err));
+  // };
 
   return (
     <Box maxW="md" m={{ sm: 5, md: "auto" }}>
@@ -13,7 +22,7 @@ const FilterActivities = () => {
       <Flex>
         <Input placeholder="location..." />
         <IconButton
-         
+          // onClick={getLocation}
           icon={<SearchIcon />}
           aria-label={"search activites by location"}
         />
