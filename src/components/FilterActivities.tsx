3ec87d2e116d038cc304337f4f2edcf983c20480
Enrico <86821724+enricogarozzo@users.noreply.github.com>
2023-03-27 15:04:25 +0200

    added assets folder

--- src/components/FilterActivities.tsx
+++ src/components/FilterActivities.tsx
@@ -1,18 +1,22 @@
-import { SearchIcon } from '@chakra-ui/icons';
-import { Box, Flex, Heading, IconButton, Input } from '@chakra-ui/react';
+import { SearchIcon } from "@chakra-ui/icons";
+import { Box, Flex, Heading, IconButton, Input } from "@chakra-ui/react";
 
 const FilterActivities = () => {
-  const getLocation = async () => {};
+  
 
   return (
-    <Box maxW="md" m={{ sm: 5, md: 'auto' }}>
+    <Box maxW="md" m={{ sm: 5, md: "auto" }}>
       <Heading textAlign="center" mb={5}>
         Choose a location
       </Heading>
 
       <Flex>
         <Input placeholder="location..." />
-        <IconButton onClick={getLocation} icon={<SearchIcon />} aria-label={'search activites by location'} />
+        <IconButton
+         
+          icon={<SearchIcon />}
+          aria-label={"search activites by location"}
+        />
       </Flex>
     </Box>
   );
