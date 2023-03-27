a5d584366f953fe469bdf651e23bf2dc8bd90d37
Joakim Myhre Wibe <71314249+JoakimWibe@users.noreply.github.com>
2023-03-27 13:37:39 +0200

    removed faulty request

--- src/components/FilterActivities.tsx
+++ src/components/FilterActivities.tsx
@@ -1,24 +1,8 @@
 import { SearchIcon } from '@chakra-ui/icons';
 import { Box, Flex, Heading, IconButton, Input } from '@chakra-ui/react';
-import axios from 'axios';
-// import { useContext, useState } from 'react';
-// import { TripContext } from '../context/Context';
 
 const FilterActivities = () => {
-//   const { activites, setActivities, location, setLocation }: any = useContext(TripContext);
-//   const [inputValue, setInputValue] = useState<string>('');
-
-  const getLocation = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.content.tripadvisor.com/api/v1/location/search?key=7C23E6E7D20B4DCF96588CBA6859738D&searchQuery=London&language=en'`
-      );
-      console.log(response.data);
-    //   setLocation(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const getLocation = async () => {};
 
   return (
     <Box maxW="md" m={{ sm: 5, md: 'auto' }}>
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-// export const proxy = 'https://floating-spire-99250.herokuapp.com/';
+export const proxy = 'https://floating-spire-99250.herokuapp.com/';
 
 export const tripAdvisorLocation = 'https://api.content.tripadvisor.com/api/v1/location/search';
 export const tripAdvisorKey = '7C23E6E7D20B4DCF96588CBA6859738D';
