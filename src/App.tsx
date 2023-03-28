b7344bbe77e6b7771755558315596161bf9e7049
Cristina Nunu <cristina.nunu@appliedtechnology.se>
2023-03-28 08:24:01 +0200

    created call to get endpoint plan and activities

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { TripContext } from './context/Context';
@@ -9,10 +10,37 @@ import Plan from './pages/Plan';
 
 function App() {
   const [activities, setActivities] = useState([]);
+  const [plans, setPlans] = useState([]);
   const [location, setLocation] = useState({});
 
+  const client = axios.create({
+    baseURL: 'https://epjctripapi.azurewebsites.net/api',
+  })
+  
+  async function getPlan() {
+    try {
+      const response = await client.get('/Plans');
+      setPlans(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  async function getActivities() {
+    try {
+      const response = await client.get('/Activities');
+      setActivities(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getPlan();
+    getActivities();
+  }, [])
+
   return (
-    <TripContext.Provider value={{ activities, setActivities, location, setLocation }}>
+    <TripContext.Provider value={{ activities, setActivities, plans, location, setLocation }}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
