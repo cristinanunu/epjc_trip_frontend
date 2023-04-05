c65f13ffe6939ee709c2aaaf7f9b500edd05946a
Joakim Myhre Wibe <71314249+JoakimWibe@users.noreply.github.com>
2023-04-05 15:47:14 +0200

    removed filter method on plan page

--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,9 +1,7 @@
 import { Heading, Divider, Button, useDisclosure, Flex } from '@chakra-ui/react';
-// import moment from 'moment';
 import { useContext, useEffect } from 'react';
 import PlanForm from '../components/PlanForm';
 import { postPlan, userUrl } from '../constants/api';
-// import AddedActivitiyCard from '../components/AddedActivitiyCard';
 import axios from 'axios';
 import PlanCard from '../components/PlanCard';
 import { TripContext } from '../context/Context';
@@ -42,9 +40,6 @@ const Plan = () => {
     }
   };
 
-  // const [plans, setPlans] = useState<SavedPlan[]>([]);
-  // const [selectedPlan, setSelectedPlan] = useState<SavedPlan | undefined>(undefined);
-
   const savePlan = async (plan: NewPlan) => {
     const saveAPlan = await postPlan(plan);
     if (saveAPlan) {
@@ -53,11 +48,6 @@ const Plan = () => {
   };
 
   const { onOpen, onClose, isOpen } = useDisclosure();
-
-  // const handleOnClick = (plan: SavedPlan) => {
-  //   setSelectedPlan(plan);
-  //   onOpen();
-  // };
 
   return (
     <Flex pt={40} direction={'column'} maxW={'3xl'} mx={{ sm: 5, md: 'auto' }}>
@@ -72,8 +62,8 @@ const Plan = () => {
 
       <Divider mb={4} />
 
-      <Flex>
-        {plans.filter((planByUser: any) => planByUser.userId === userId).map((plan: any) => (
+      <Flex direction={'column'}>
+        {plans.map((plan: any) => (
           <PlanCard plan={plan} />
         ))}
       </Flex>
