087fdece24c8e3d50f1fddd1076b130e30b1d53f
Cristina Nunu <nunucristina0@gmail.com>
2023-04-28 19:35:40 +0200

    fixed bugs in forms

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -5,7 +5,6 @@ import { SavedPlan } from '../pages/Plan';
 export const epjcAttractions = 'https://epjcattractions.azurewebsites.net/api/Activities?location=';
 export const travelAdvisorSearchUrl = 'https://travel-advisor.p.rapidapi.com/locations/search';
 export const traveAdvisorAttractionsUrl = 'https://travel-advisor.p.rapidapi.com/attractions/list';
-export const apiKey = '4887eda678mshdc0804b3f912ba8p11db58jsn1475c5f5dc16';
 export const apiHost = 'travel-advisor.p.rapidapi.com';
 export const loginUrl = 'https://epjc-trip.azurewebsites.net/login';
 export const registerUrl = 'https://epjc-trip.azurewebsites.net/register';
